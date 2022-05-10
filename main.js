34694ae8e659d163a2d1bab44596ac1255a93f4f
carubiodlo <46832879+carubiodlo@users.noreply.github.com>
2022-05-10 16:21:02 -0500

    Initial test for AR

---
+++ main.js
@@ -0,0 +1,35 @@
+import {loadGLTF} from "../../applications-20220402/applications/libs/loader.js"
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const start = async () => {
+        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            container: document.body,
+            imageTargetSrc: '../../applications-20220402/applications/assets/targets/musicband.mind'
+        });
+        const {renderer, scene, camera} = mindarThree;
+
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
+
+        const raccoon = await loadGLTF("../../applications-20220402/applications/assets/models/musicband-raccoon/scene.gltf");
+        raccoon.scene.scale.set(0.1, 0.1, 0.1);
+        raccoon.scene.position.set(0, -0.4, 0);
+
+        const bear = await loadGLTF("../../applications-20220402/applications/assets/models/musicband-bear/scene.gltf");
+        bear.scene.scale.set(0.1, 0.1, 0.1);
+        bear.scene.position.set(0, -0.4, 0);
+
+        const raccoonAnchor = mindarThree.addAnchor(0);
+        raccoonAnchor.group.add(raccoon.scene);
+
+        const bearAnchor = mindarThree.addAnchor(1);
+        bearAnchor.group.add(bear.scene);
+ 
+        await mindarThree.start();
+        renderer.setAnimationLoop(() => {
+            renderer.render(scene, camera);
+        });
+    }
+    start();
+});
